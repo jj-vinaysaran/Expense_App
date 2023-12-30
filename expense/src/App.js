241d251fee97f61fcca018d58192cce8033430b6
jj-vinaysaran <vinayjayjay12@gmail.com>
2023-12-30 08:04:25 +0530

    Done

---
+++ expense/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Component } from "react";
+import Expanse from "./Components/head";
+import './App.css';
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="main">
+        <div>
+          <Expanse />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+export default App;
